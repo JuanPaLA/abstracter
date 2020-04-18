2d652953df2b681ad621a6308a74fbb99d04a2b0
JuanPaLA <juanlopezalurralde@gmail.com>
2020-04-18 20:37:20 -0300

    15

--- server.js
+++ server.js
@@ -21,14 +21,18 @@ const abstracts = require('./routes/abstracts')
 app.use('/api', abstracts)
 
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 app.listen(port, () => {
