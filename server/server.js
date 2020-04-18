a64b2709ef4527477f00e1a1633336ca090a0d4f
JuanPaLA <juanlopezalurralde@gmail.com>
2020-04-18 17:04:34 -0300

    fourth commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 var app = express();
+const path = require('path')
 
 app.use(bodyParser());
 app.use(cors());
@@ -18,6 +19,13 @@ const port = process.env.PORT || 5000;
 
 const abstracts = require('./routes/abstracts')
 app.use('/api', abstracts)
+
+if(process.env.NODE_ENV === 'production')
+//set static folder
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 app.listen(port, () => {
 console.log("Server is running on " + port + "port");
