eeaa66432d4180eeb11b76a007daecd4fa258ed4
JuanPaLA <juanlopezalurralde@gmail.com>
2020-04-18 19:11:23 -0300

    10

--- server.js
+++ server.js
@@ -20,12 +20,16 @@ const port = process.env.PORT || 5000;
 const abstracts = require('./routes/abstracts')
 app.use('/api', abstracts)
 
-if(process.env.NODE_ENV === 'production')
-//set static folder
-app.use(express.static('client/build'))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
 console.log("Server is running on " + port + "port");
