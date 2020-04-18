0019da1a3ee6f01ceda6ad5fd32537799b768799
JuanPaLA <juanlopezalurralde@gmail.com>
2020-04-18 17:24:22 -0300

    fith commit

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.use(bodyParser());
 app.use(cors());
+const path = require('path')
 
 //mongodb
 const db = require('./config/keys').mongoURI;
@@ -18,6 +19,13 @@ const port = process.env.PORT || 5000;
 
 const abstracts = require('./routes/abstracts')
 app.use('/api', abstracts)
+
+if(process.env.NODE_ENV === 'production')
+//set static folder
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 app.listen(port, () => {
 console.log("Server is running on " + port + "port");
